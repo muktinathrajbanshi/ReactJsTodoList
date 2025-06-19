559f309c510b415c9cc93959e913315d29aa3458
unknown <muktinathrajbanshi786@gmail.com>
2025-06-19 15:15:14 +0545

    my new react js project based on ToDoList

---
+++ src/component/todo.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import todoimg from "../images/todoimage.png";
+import "../App.css";
+
+const Todo = () => {
+  return (
+    <>
+      <div className="main_div">
+        <div className="child_div">
+            <figure>
+                <img src={todoimg} alt="todologo" />
+                <figcaption>Add Your List Here ✌️</figcaption>
+            </figure>
+
+            <div className="addItems">
+                <input type="text" placeholder="✍️ Add Items..." />
+                <i className="fa-solid fa-plus add-btn" title="Add Item"></i>
+            </div>
+
+            <div className="showItems">
+                <div className="eachItem">
+                    <h3>apple</h3>
+                    <i className="fa-solid fa-trash delete-btn" title="Delete Item"></i>
+                </div>
+            </div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
