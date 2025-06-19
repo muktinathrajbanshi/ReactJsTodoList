f59cf7f4e083dcf9e695e15c4b1feebc1579f7dd
unknown <muktinathrajbanshi786@gmail.com>
2025-06-19 19:33:45 +0545

    half react and half css is done for TodoList

--- src/component/todo.js
+++ src/component/todo.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import todoimg from "../images/todoimage.png";
 import "../App.css";
 
 const Todo = () => {
+
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    if(!inputData){
+
+    }else {
+      setItems([...items, inputData]);
+      setInputData("")
+    }
+
+  }
+  // addItem();
+
   return (
     <>
       <div className="main_div">
@@ -13,15 +28,27 @@ const Todo = () => {
             </figure>
 
             <div className="addItems">
-                <input type="text" placeholder="✍️ Add Items..." />
-                <i className="fa-solid fa-plus add-btn" title="Add Item"></i>
+                <input type="text" placeholder="✍️ Add Items..." 
+                  value={inputData}
+                  onChange={(event) => setInputData(event.target.value) }
+                />
+                <i className="fa-solid fa-plus add-btn" title="Add Item" onClick={addItem}></i>
             </div>
 
             <div className="showItems">
-                <div className="eachItem">
-                    <h3>apple</h3>
-                    <i className="fa-solid fa-trash delete-btn" title="Delete Item"></i>
-                </div>
+
+                {
+                  items.map((elem, ind) => {
+                    return (
+                       <div className="eachItem">
+                        <h3>{ elem }</h3>
+                        <i className="fa-solid fa-trash delete-btn" title="Delete Item"></i>
+                      </div>
+                    )
+                  })
+                }
+
+               
             </div>
             <div className="showItems">
               <button className="btn effect-04" data-sm-link-text="Remove All"><span>CHECK LIST</span></button>
