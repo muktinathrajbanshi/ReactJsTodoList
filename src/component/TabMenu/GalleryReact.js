bc1b86b57ee6bdaa5926169b1a2965e1c1327f5c
unknown <muktinathrajbanshi786@gmail.com>
2025-06-21 16:33:31 +0545

    So, dynamically get the data from API

--- src/component/TabMenu/GalleryReact.js
+++ src/component/TabMenu/GalleryReact.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../TabMenu/tab.css";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-// import Menu from './menu';
+import Menu from './menu';
 
 const GalleryReact = () => {
+
+  const [items, setItems] = useState(Menu);
+
   return (
     <>
-      <h1 className="mt-5 text-center main-heading">Order Your Favourite Dish</h1>
+      <h1 className="mt-5 mb-5 text-center main-heading">Order Your Favourite Dish</h1>
       <hr />
       <div className="menu-tabs container">
             <div className="menu-tab d-flex justify-content-around">
@@ -18,34 +21,45 @@ const GalleryReact = () => {
 
             </div>
       </div>
+      {/* my main items section */}
       <div className="menu-items container-fluid mt-5">
         <div className="row">
           <div className="col-11 mx-auto">
             <div className="row my-5">
-              <div className="item1 col-12 col-md-6 col-lg-6 col-xl-4">
-                <div className="row Item-inside">
-                    {/* for image */}
-                  <div className="col-12 col-md-12 col-lg-4 img-div">
-                    <img src="" alt="menuPic" className="img-fluid" />
-                  </div>
-                  {/* menu item description */}
-                  <div className="col-12 col-md-12 col-lg-8">
-                    <div className="main-title pt-4 pb-3">
-                      <h1>MAGGI</h1>
-                      <p>hello this is maggi just test it and feel it you would be happy.</p>
-                    </div>
-                    <div className="menu-price-book">
-                      <div className="price-book-divide d-flex justify-content-between">
-                        <h2>Price : Rs.12</h2>
-                        <a href="#">
-                          <button className="btn btn-primary">Order Now</button>
-                        </a>
+
+            {
+              items.map((elem) => {
+                const {id, name, image, description, price} = elem;
+
+                return(
+                        <div className="item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5">
+                    <div className="row Item-inside mx-1">
+                        {/* for image */}
+                      <div className="col-12 col-md-12 col-lg-4 img-div">
+                        <img src={image} alt={name} className="img-fluid" />
                       </div>
-                      <p>*Prices may vary on selected date.</p>
+                      {/* menu item description */}
+                      <div className="col-12 col-md-12 col-lg-8">
+                        <div className="main-title pt-4 pb-3">
+                          <h1>{name}</h1>
+                          <p>{description}</p>
+                        </div>
+                        <div className="menu-price-book">
+                          <div className="price-book-divide d-flex justify-content-between">
+                            <h2>Price : {price}</h2>
+                            <a href="#">
+                              <button className="btn btnUpdate">Order Now</button>
+                            </a>
+                          </div>
+                          <p>*Prices may vary on selected date.</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                )
+              })
+            }
+
             </div>
           </div>
         </div>
